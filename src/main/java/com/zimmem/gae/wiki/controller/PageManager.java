f4344dcce015e9149a75b61c2b9ef2c5a43cd514
Zimmem <zimmem@gmail.com>
2012-12-01 10:16:06 +0800

    full page editor

--- src/main/java/com/zimmem/gae/wiki/controller/PageManager.java
+++ src/main/java/com/zimmem/gae/wiki/controller/PageManager.java
@@ -2,6 +2,9 @@ package com.zimmem.gae.wiki.controller;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.Properties;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,9 +23,10 @@ public class PageManager {
     private WikiPageService wikpagePageService;
 
     @RequestMapping(value = "/post_page", method = RequestMethod.POST)
-    public String postPage(WikiPage wikiPage) throws  IOException {
+    public String postPage(WikiPage wikiPage) throws IOException {
         MarkdownProcessor process = new MarkdownProcessor();
         wikiPage.setHtml(process.markdown(wikiPage.getWiki()));
+        wikiPage.setTitle(parseFirstLine(wikiPage.getWiki()));
         wikpagePageService.saveWikiPage(wikiPage);
         return "redirect:/page?id=" + wikiPage.getId();
     }
@@ -42,4 +46,30 @@ public class PageManager {
         model.put("wikiPage", page);
         return "/pageForm";
     }
+
+    private String parseFirstLine(String text) {
+        int index = text.indexOf("\r\n");
+        if (index < 0) {
+            index = text.indexOf("\n");
+        }
+
+        if (index < 0) {
+            return text;
+        }
+        return text.substring(0, index);
+
+    }
+
+    public static void main(String[] args) {
+        String text = "\n\n    sdfsf\n    sdfsdf\n\n";
+        Pattern pattern = Pattern.compile("(?:\\n\\n|\\A)((?:(?:[ ]{4}).*\\n+)+)((?=^[ ]{0,4}\\S)|\\Z)", Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(text);
+       if(matcher.find()){
+        System.out.println(matcher.group());
+       }
+        MarkdownProcessor processor = new MarkdownProcessor();
+        String markdown = processor.markdown("\r\n\n    sdfsf\n    sdfsdf\n\n");
+        System.out.println(markdown);
+    }
+
 }
